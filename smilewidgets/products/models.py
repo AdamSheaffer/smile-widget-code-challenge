a83ce57fc821e570c80af39f9f167a70040b0faa
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2018-10-06 10:01:31 -0500

    move price to ProductPrice, install DRF, add fixture

--- smilewidgets/products/models.py
+++ smilewidgets/products/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-
+from datetime import date
 
 class Product(models.Model):
     name = models.CharField(max_length=25, help_text='Customer facing name of product')
     code = models.CharField(max_length=10, help_text='Internal facing reference to product')
-    price = models.PositiveIntegerField(help_text='Price of product in cents')
     
     def __str__(self):
         return '{} - {}'.format(self.name, self.code)
@@ -22,3 +21,16 @@ class GiftCard(models.Model):
     @property
     def formatted_amount(self):
         return '${0:.2f}'.format(self.amount / 100)
+
+
+# PriceSchedule allows us to denormalize things like description and dates
+class PriceSchedule(models.Model):
+    date_start = models.DateField(default=date.min)
+    date_end = models.DateField(default=date.max)
+    description = models.CharField(max_length=50, help_text='Customer facing price name')
+
+
+class ProductPrice(models.Model):
+    product = models.ForeignKey(Product, related_name='product_price', on_delete=models.CASCADE)
+    price_schedule = models.OneToOneField(PriceSchedule, on_delete=models.CASCADE, primary_key=True)
+    price = models.PositiveIntegerField(help_text='Price of product in cents')
