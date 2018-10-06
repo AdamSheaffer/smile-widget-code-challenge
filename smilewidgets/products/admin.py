81e5dd39e311c9b5913802f3e9aec461bd5b6d81
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2018-10-06 10:39:15 -0500

    add migration, register models with admin

--- smilewidgets/products/admin.py
+++ smilewidgets/products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    GiftCard,
+    PriceSchedule,
+    Product,
+    ProductPrice
+)
+
+admin.site.register(GiftCard)
+admin.site.register(PriceSchedule)
+admin.site.register(Product)
+admin.site.register(ProductPrice)
